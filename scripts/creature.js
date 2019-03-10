f9884ca7d209cb38eca48e4ff92bf748f3e8e6f8
Anna Peterson <peterson.anna.c@gmail.com>
2019-03-09 16:14:13 -0800

    Remove extra getters and setters from classes

--- scripts/creature.js
+++ scripts/creature.js
@@ -3,37 +3,13 @@ class Creature {
   constructor() {
     this.$ = $('<span></span>');
   }
-
-  set $(element) {
-    this._element = element;
-  }
-
-  get $() {
-    return this._element;
-  }
-
-  set direction(newDirection) {
-    this._direction = newDirection;
-  }
-
-  get direction() {
-    return this._direction;
-  }
-
-  set position(array) {
-    this._position = array;
-  }
-
-  get position() {
-    return this._position || [];
-  }
 }
 
 class Pacman extends Creature {
   constructor () {
     super();
 
-    this._direction = 'right';
+    this.direction = 'right';
     this.$.text('P');
   }
 }
