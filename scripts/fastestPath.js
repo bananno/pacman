bd5adf064fb9a0b5b703a0d6e47c8e2c128ff9f5
Anna Peterson <peterson.anna.c@gmail.com>
2019-03-10 16:36:12 -0700

    Continue building fastest path function

--- scripts/fastestPath.js
+++ scripts/fastestPath.js
@@ -1,19 +1,47 @@
 
 function fastestPath([r1, c1], [r2, c2]) {
   $('td').removeClass('PATH-TEMP');
+  $('td').removeClass('PATH-TEMP-END');
 
-  board[r1][c1].$.addClass('PATH-TEMP');
-  board[r2][c2].$.addClass('PATH-TEMP');
+  let [i, j] = [r1, c1];
+  let safety = 0;
 
-  if (r1 == r2) {
-    for (let i = c1; i <= c2; i++) {
-      board[r1][i].$.addClass('PATH-TEMP');
+  const directions = ['right', 'down', 'left', 'up'];
+  let d = 0;
+
+  const diff = {
+    right: [0, 1],
+    left: [0, -1],
+    up: [-1, 0],
+    down: [1, 0],
+  };
+
+  while (true) {
+
+    if (board[i][j].wall || board[i][j].$.hasClass('PATH-TEMP')) {
+      i -= diff[directions[d]][0];
+      j -= diff[directions[d]][1];
+
+      d += 1;
+      if (d > 3) {
+        d = 0;
+      }
+    }
+
+    board[i][j].$.addClass('PATH-TEMP');
+
+    i += diff[directions[d]][0];
+    j += diff[directions[d]][1];
+
+
+
+    safety += 1;
+    if (safety > 100) {
+      console.log('safety');
+      break;
     }
   }
 
-  if (c1 == c2) {
-    for (let i = r1; i <= r2; i++) {
-      board[i][c1].$.addClass('PATH-TEMP');
-    }
-  }
+  board[r1][c1].$.addClass('PATH-TEMP-END');
+  board[r2][c2].$.addClass('PATH-TEMP-END');
 }
