9fec1458f23821eb1c259d9d3d31b9f762da47c8
Anna Peterson <peterson.anna.c@gmail.com>
2019-03-11 14:03:42 -0700

    Add more testing functions

--- scripts/init.js
+++ scripts/init.js
@@ -15,9 +15,11 @@ function newGame() {
 
   createBoard();
 
-  // $('#board').append('<button onClick="TEST_movePacman()">move pacman</button>');
+  $('#board').append('<button onClick="TEST_toggleGrid()">toggle grid</button><br>');
 
-  $('#board').append('<button onClick="TEST_toggleGrid()">toggle grid</button>');
+  $('#board').append('<button onClick="TEST_movePacman()">move pacman</button>');
+  $('#board').append('<button onClick="TEST_moveGhostLeft()">move ghost left</button>');
+  $('#board').append('<button onClick="TEST_moveGhostRight()">move ghost right</button><br>');
 
   $('#board').append('<button onClick="fastestPath([5, 3], [5, 10])">path1</button>');
   $('#board').append('<button onClick="fastestPath([3, 6], [12, 6])">path2</button>');
@@ -26,6 +28,14 @@ function newGame() {
 
 function TEST_movePacman() {
   pacman.position = [11, 10];
+}
+
+function TEST_moveGhostLeft() {
+  ghosts[0].position = [14, 3];
+}
+
+function TEST_moveGhostRight() {
+  ghosts[1].position = [14, 25];
 }
 
 function TEST_toggleGrid() {
