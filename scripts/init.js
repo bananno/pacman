df5d033caa117785cec28c2061473bf7a9d43447
Anna Peterson <peterson.anna.c@gmail.com>
2019-03-11 14:17:08 -0700

    Move test buttons to own file

--- scripts/init.js
+++ scripts/init.js
@@ -15,15 +15,7 @@ function newGame() {
 
   createBoard();
 
-  $('#board').append('<button onClick="TEST_toggleGrid()">toggle grid</button><br>');
-
-  $('#board').append('<button onClick="TEST_movePacman()">move pacman</button>');
-  $('#board').append('<button onClick="TEST_moveGhostLeft()">move ghost left</button>');
-  $('#board').append('<button onClick="TEST_moveGhostRight()">move ghost right</button><br>');
-
-  $('#board').append('<button onClick="fastestPath([5, 3], [5, 10])">path1</button>');
-  $('#board').append('<button onClick="fastestPath([3, 6], [12, 6])">path2</button>');
-  $('#board').append('<button onClick="fastestPath([5, 3], [12, 6])">path3</button>');
+  addTestButtons();
 }
 
 function startGame() {
@@ -36,24 +28,4 @@ function startGame() {
   ghosts.forEach(ghost => {
     ghost.start();
   });
-}
-
-function TEST_movePacman() {
-  pacman.position = [11, 10];
-}
-
-function TEST_moveGhostLeft() {
-  ghosts[0].position = [14, 3];
-}
-
-function TEST_moveGhostRight() {
-  ghosts[1].position = [14, 25];
-}
-
-function TEST_toggleGrid() {
-  if ($('#board').hasClass('normal')) {
-    $('#board').removeClass('normal');
-  } else {
-    $('#board').addClass('normal');
-  }
 }
