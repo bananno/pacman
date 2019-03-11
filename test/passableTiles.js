10e6139155ce04c0df4a33bcf69355fa3ed12fbe
Anna Peterson <peterson.anna.c@gmail.com>
2019-03-11 16:21:44 -0700

    Begin creating tests

---
+++ test/passableTiles.js
@@ -0,0 +1,28 @@
+
+const testMap = [
+  '||||',
+  '|  |',
+  '|P |',
+  '|  |',
+  '||||',
+];
+
+const testGame = new Game(testMap, true);
+
+console.log(testGame);
+
+console.log('Wall is not passable for any creature');
+console.log(testGame.isTilePassable(2, 0, null));
+
+console.log('Wall is not passable for Pacman');
+console.log(testGame.isTilePassable(2, 0, testGame.pacman));
+
+console.log('Empty space is passable for any creature');
+console.log(testGame.isTilePassable(2, 1, null));
+
+console.log('Empty space is passable for Pacman');
+console.log(testGame.isTilePassable(2, 1, testGame.pacman));
+
+(function () {
+  console.log('testing...')
+})();
