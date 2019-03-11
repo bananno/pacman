9abe7c152f8b9cc5da463be7c48fcae58804c7db
Anna Peterson <peterson.anna.c@gmail.com>
2019-03-11 16:27:54 -0700

    Continue creating tests

--- test/passableTiles.js
+++ test/passableTiles.js
@@ -1,28 +1,26 @@
 
-const testMap = [
-  '||||',
-  '|  |',
-  '|P |',
-  '|  |',
-  '||||',
-];
+new Test(() => {
+  const testMap = [
+    '||||',
+    '|  |',
+    '|P |',
+    '|  |',
+    '||||',
+  ];
 
-const testGame = new Game(testMap, true);
+  const testGame = new Game(testMap, true);
 
-console.log(testGame);
+  console.log(testGame);
 
-console.log('Wall is not passable for any creature');
-console.log(testGame.isTilePassable(2, 0, null));
+  console.log('Wall is not passable for any creature');
+  console.log(testGame.isTilePassable(2, 0, null));
 
-console.log('Wall is not passable for Pacman');
-console.log(testGame.isTilePassable(2, 0, testGame.pacman));
+  console.log('Wall is not passable for Pacman');
+  console.log(testGame.isTilePassable(2, 0, testGame.pacman));
 
-console.log('Empty space is passable for any creature');
-console.log(testGame.isTilePassable(2, 1, null));
+  console.log('Empty space is passable for any creature');
+  console.log(testGame.isTilePassable(2, 1, null));
 
-console.log('Empty space is passable for Pacman');
-console.log(testGame.isTilePassable(2, 1, testGame.pacman));
-
-(function () {
-  console.log('testing...')
-})();
+  console.log('Empty space is passable for Pacman');
+  console.log(testGame.isTilePassable(2, 1, testGame.pacman));
+});
