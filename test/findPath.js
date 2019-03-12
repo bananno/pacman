a9c90bc3f029dd6c80324336f48eaa2c66d31319
Anna Peterson <peterson.anna.c@gmail.com>
2019-03-12 16:01:09 -0700

    Add tests

--- test/findPath.js
+++ test/findPath.js
@@ -1,35 +1,60 @@
 
 addTest(test => {
   test.map = [
-    '    ', // 0,0  0,1  0,2  0,3
-    '    ', // 1,0  1,1  1,2  1,3
-    '    ', // 2,0  2,1  2,2  2,3
-    '    ', // 3,0  3,1  3,2  3,3
+    '   ', // 0,0  0,1  0,2
+    '   ', // 1,0  1,1  1,2
+    '   ', // 2,0  2,1  2,2
   ];
 
   // DIRECT PATH
 
   test.check(
     'path is directly up when possible',
-    [[3, 1], [2, 1], [1, 1], [0, 1]],
-    test.game.findPath([3, 1], [0, 1])
-  );
-
-  test.check(
-    'path is directly right when possible',
-    [[1, 0], [1, 1], [1, 2], [1, 3]],
-    test.game.findPath([1, 0], [1, 3])
+    [[2, 1], [1, 1], [0, 1]],
+    test.game.findPath([2, 1], [0, 1])
   );
 
   test.check(
     'path is directly left when possible',
-    [[1, 3], [1, 2], [1, 1], [1, 0]],
-    test.game.findPath([1, 3], [1, 0])
+    [[1, 2], [1, 1], [1, 0]],
+    test.game.findPath([1, 2], [1, 0])
+  );
+
+  test.check(
+    'path is directly right when possible',
+    [[1, 0], [1, 1], [1, 2]],
+    test.game.findPath([1, 0], [1, 2])
   );
 
   test.check(
     'path is directly down when possible',
-    [[0, 1], [1, 1], [2, 1], [3, 1]],
-    test.game.findPath([0, 1], [3, 1])
+    [[0, 1], [1, 1], [2, 1]],
+    test.game.findPath([0, 1], [2, 1])
+  );
+
+  // GHOST HOUSE
+
+  test.game.tile(1, 1).house = true;
+  test.check(
+    'path will not enter ghost house tile if destination is not in house',
+    [[1, 0], [0, 0], [0, 1], [0, 2], [1, 2]],
+    test.game.findPath([1, 0], [1, 2])
+  );
+
+  test.game.tile(1, 1).house = true;
+  test.game.tile(1, 2).house = true;
+  test.check(
+    'path will enter ghost house tile if destination is in house',
+    [[1, 0], [1, 1], [1, 2]],
+    test.game.findPath([1, 0], [1, 2])
+  );
+
+  test.game.tile(1, 0).house = true;
+  test.game.tile(1, 1).house = true;
+  test.game.tile(1, 2).house = false;
+  test.check(
+    'path will enter ghost house tile if origin is in house',
+    [[1, 0], [1, 1], [1, 2]],
+    test.game.findPath([1, 0], [1, 2])
   );
 });
