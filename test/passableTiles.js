51dfea5cd08e8fd672835bf30ac196ba5998f31a
Anna Peterson <peterson.anna.c@gmail.com>
2019-03-11 17:12:11 -0700

    Add more tests

--- test/passableTiles.js
+++ test/passableTiles.js
@@ -1,16 +1,24 @@
 
 addTest(test => {
   test.map = [
-    '||||',
-    '|  |',
-    '|P |',
-    '|  |',
-    '||||',
+    '||||||',
+    '|    |',
+    '|P   |',
+    '|    |',
+    '|G   |',
+    '|    |',
+    '|    |',
+    '||||||',
   ];
 
   test.check(
     'Wall is not passable for any creature', false,
-    test.game.isTilePassable(2, 0, null)
+    test.game.isTilePassable(1, 0, null)
+  );
+
+  test.check(
+    'Empty space is passable for any creature', true,
+    test.game.isTilePassable(1, 3, null)
   );
 
   test.check(
@@ -19,12 +27,17 @@ addTest(test => {
   );
 
   test.check(
-    'Empty space is passable for any creature', true,
-    test.game.isTilePassable(2, 1, null)
+    'Empty space is passable for pacman', true,
+    test.game.isTilePassable(2, 2, test.game.pacman)
   );
 
   test.check(
-    'Empty space is passable for Pacman', true,
-    test.game.isTilePassable(2, 1, test.game.pacman)
+    'Wall is not passable for ghost', false,
+    test.game.isTilePassable(4, 0, test.game.ghosts[0])
+  );
+
+  test.check(
+    'Empty space is passable for ghost', true,
+    test.game.isTilePassable(4, 2, test.game.ghosts[0])
   );
 });
