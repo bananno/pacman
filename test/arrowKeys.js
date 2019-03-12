72f3c005df78bb1566d69d95c2d56210a3b008cf
Anna Peterson <peterson.anna.c@gmail.com>
2019-03-11 19:24:12 -0700

    Add more arrow key tests

--- test/arrowKeys.js
+++ test/arrowKeys.js
@@ -88,3 +88,103 @@ addTest(test => {
     test.game.pacman.direction
   );
 });
+
+addTest(test => {
+  test.map = [
+    '   ', // 0,0  0,1  0,2
+    '   ', // 1,0  1,1  1,2
+    '   ', // 2,0  2,1  2,2
+  ];
+
+  // UP
+
+  test.game.pacman.position = [0, 1];
+  test.game.pacman.direction = 'right';
+  test.game.board[2][1].wall = false;
+  test.game.pressKey({ key: 'ArrowUp' });
+  test.check(
+    'up arrow key turns pacman when up wrap-around tile is passable',
+    'up',
+    test.game.pacman.direction
+  );
+
+  test.game.pacman.position = [0, 1];
+  test.game.pacman.direction = 'right';
+  test.game.board[2][1].wall = true;
+  test.game.pressKey({ key: 'ArrowUp' });
+  test.check(
+    'up arrow key does nothing when up wrap-around tile is not passable',
+    'right',
+    test.game.pacman.direction
+  );
+  test.game.board[2][1].wall = false;
+
+  // LEFT
+
+  test.game.pacman.position = [1, 0];
+  test.game.pacman.direction = 'right';
+  test.game.board[1][2].wall = false;
+  test.game.pressKey({ key: 'ArrowLeft' });
+  test.check(
+    'left arrow key turns pacman when left wrap-around tile is passable',
+    'left',
+    test.game.pacman.direction
+  );
+
+  test.game.pacman.position = [1, 0];
+  test.game.pacman.direction = 'right';
+  test.game.board[1][2].wall = true;
+  test.game.pressKey({ key: 'ArrowLeft' });
+  test.check(
+    'left arrow key does nothing when left wrap-around tile is not passable',
+    'right',
+    test.game.pacman.direction
+  );
+  test.game.board[1][2].wall = false;
+
+  // RIGHT
+
+  test.game.pacman.position = [1, 2];
+  test.game.pacman.direction = 'up';
+  test.game.board[1][0].wall = false;
+  test.game.pressKey({ key: 'ArrowRight' });
+  test.check(
+    'right arrow key turns pacman when right wrap-around tile is passable',
+    'right',
+    test.game.pacman.direction
+  );
+
+  test.game.pacman.position = [1, 2];
+  test.game.pacman.direction = 'up';
+  test.game.board[1][0].wall = true;
+  test.game.pressKey({ key: 'ArrowRight' });
+  test.check(
+    'right arrow key does nothing when right wrap-around tile is not passable',
+    'up',
+    test.game.pacman.direction
+  );
+  test.game.board[1][0].wall = false;
+
+  // DOWN
+
+  test.game.pacman.position = [2, 1];
+  test.game.pacman.direction = 'right';
+  test.game.board[0][1].wall = false;
+  test.game.pressKey({ key: 'ArrowDown' });
+  test.check(
+    'down arrow key turns pacman when down wrap-around tile is passable',
+    'down',
+    test.game.pacman.direction
+  );
+
+  test.game.pacman.position = [2, 1];
+  test.game.pacman.direction = 'right';
+  test.game.board[0][1].wall = true;
+  test.game.pressKey({ key: 'ArrowDown' });
+  test.check(
+    'down arrow key does nothing when down wrap-around tile is not passable',
+    'right',
+    test.game.pacman.direction
+  );
+  test.game.board[0][1].wall = false;
+});
