f3ba9c57cde2b732b6049a0a2059905bd2dba6fa
Anna Peterson <peterson.anna.c@gmail.com>
2019-03-11 21:46:58 -0700

    Move global constants to different file

--- scripts/game/direction.js
+++ scripts/game/direction.js
@@ -1,18 +1,4 @@
 
-const oppositeDirection = {
-  left: 'right',
-  right: 'left',
-  up: 'down',
-  down: 'up'
-};
-
-const positionAdjustment = {
-  left: [0, -1],
-  right: [0, 1],
-  up: [-1, 0],
-  down: [1, 0]
-};
-
 Game.prototype.getNextPosition = function(direction, oldPosition) {
   if (direction == null || oldPosition.length == 0) {
     return null;
--- scripts/init.js
+++ scripts/init.js
@@ -1,4 +1,18 @@
 
+const oppositeDirection = {
+  left: 'right',
+  right: 'left',
+  up: 'down',
+  down: 'up'
+};
+
+const positionAdjustment = {
+  left: [0, -1],
+  right: [0, 1],
+  up: [-1, 0],
+  down: [1, 0]
+};
+
 $(document).ready(() => {
   new Game();
 });
