cb99ba6a7e8bbdb07ae02348dea0d6eb64763ca2
Anna Peterson <peterson.anna.c@gmail.com>
2019-03-12 15:40:02 -0700

    Add tests

--- test/findPath.js
+++ test/findPath.js
@@ -7,9 +7,29 @@ addTest(test => {
     '    ', // 3,0  3,1  3,2  3,3
   ];
 
+  // DIRECT PATH
+
+  test.check(
+    'path is directly up when possible',
+    [[3, 1], [2, 1], [1, 1], [0, 1]],
+    test.game.findPath([3, 1], [0, 1])
+  );
+
   test.check(
     'path is directly right when possible',
     [[1, 0], [1, 1], [1, 2], [1, 3]],
     test.game.findPath([1, 0], [1, 3])
+  );
+
+  test.check(
+    'path is directly left when possible',
+    [[1, 3], [1, 2], [1, 1], [1, 0]],
+    test.game.findPath([1, 3], [1, 0])
+  );
+
+  test.check(
+    'path is directly down when possible',
+    [[0, 1], [1, 1], [2, 1], [3, 1]],
+    test.game.findPath([0, 1], [3, 1])
   );
 });
