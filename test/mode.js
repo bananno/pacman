8c26a93909d70002277ef7032f99fb5a3367f95d
Anna Peterson <peterson.anna.c@gmail.com>
2019-03-13 16:48:45 -0700

    Add tests for ghost mode

--- test/mode.js
+++ test/mode.js
@@ -33,4 +33,22 @@ addTest(test => {
     'scatter',
     test.game.mode
   );
+
+  test.game.scatter = true;
+  test.game.ghosts[0].mode = 'scatter';
+  test.game.toggleMode();
+  test.check(
+    'ghosts in \'scatter\' mode are updated to \'chase\' when game mode updates',
+    'chase',
+    test.game.ghosts[0].mode
+  );
+
+  test.game.scatter = false;
+  test.game.ghosts[0].mode = 'chase';
+  test.game.toggleMode();
+  test.check(
+    'ghosts in \'chase\' mode are updated to \'scatter\' when game mode updates',
+    'scatter',
+    test.game.ghosts[0].mode
+  );
 });
