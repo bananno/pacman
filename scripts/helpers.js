6211f864c0e5f9a4af49e460f6e60830eb70ade4
Anna Peterson <peterson.anna.c@gmail.com>
2019-03-13 12:02:40 -0700

    Remove now-unused helper function

--- scripts/helpers.js
+++ scripts/helpers.js
@@ -1,8 +1,4 @@
 
-function tileContainsBoth(tile) {
-  return tile.$.html().match('ghost-dangerous') && tile.$.html().match('pacman');
-}
-
 function chooseRandom(array) {
   return array[Math.floor(Math.random() * array.length)];
 }
