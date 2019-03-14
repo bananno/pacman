72fa0b0bc6c01e2aad72e64dd71a18ef47c95783
Anna Peterson <peterson.anna.c@gmail.com>
2019-03-13 17:35:16 -0700

    Add tests for encountering ghosts in different modes

--- test/encounter.js
+++ test/encounter.js
@@ -34,3 +34,50 @@ addTest(test => {
     test.game.isLost
   );
 });
+
+addTest(test => {
+  test.map = [
+    ' ',
+  ];
+
+  test.game.pacman.position = [0, 0];
+  test.game.ghosts[0].position = [0, 0];
+  test.game.ghosts[0].mode = 'blue';
+  test.game.ghosts[0].blue = true;
+  test.game.lives = 3;
+
+  test.game.encounter(0, 0);
+
+  test.check(
+    'encountered ghost in \'blue\' mode switches to \'eyes\' mode',
+    'eyes',
+    test.game.ghosts[0].mode
+  );
+
+  test.check(
+    'a life is not lost when a blue ghost is encountered',
+    3,
+    test.game.lives
+  );
+
+  test.game.pacman.position = [0, 0];
+  test.game.ghosts[0].position = [0, 0];
+  test.game.ghosts[0].mode = 'eyes';
+  test.game.ghosts[0].blue = false;
+  test.game.ghosts[0].eyes = true;
+  test.game.lives = 3;
+
+  test.game.encounter(0, 0);
+
+  test.check(
+    'encountered ghost in \'eyes\' mode is not updated',
+    'eyes',
+    test.game.ghosts[0].mode
+  );
+
+  test.check(
+    'a life is not lost when an eyes ghost is encountered',
+    3,
+    test.game.lives
+  );
+});
