317b41e3b944ebda32d15608a22de70cf81f5410
Anna Peterson <peterson.anna.c@gmail.com>
2019-03-13 19:14:08 -0700

    Move group of tests to different file

--- test/passableTiles.js
+++ test/passableTiles.js
@@ -83,3 +83,77 @@ addTest(test => {
     test.game.isTilePassable(0, 1, test.game.ghosts[0])
   );
 });
+
+// WHETHER CREATURE CAN MOVE GIVEN DIRECTION
+
+addTest(test => {
+  test.map = [
+    '   ',
+    ' P ',
+    '   ',
+  ];
+
+  // UP
+
+  test.game.tile(0, 1).wall = false;
+  test.check(
+    'creature can move up when tile is passable',
+    true,
+    test.game.pacman.canMove('up')
+  );
+
+  test.game.tile(0, 1).wall = true;
+  test.check(
+    'creature cannot move up when tile is not passable',
+    false,
+    test.game.pacman.canMove('up')
+  );
+
+  // LEFT
+
+  test.game.tile(1, 0).wall = false;
+  test.check(
+    'creature can move left when tile is passable',
+    true,
+    test.game.pacman.canMove('left')
+  );
+
+  test.game.tile(1, 0).wall = true;
+  test.check(
+    'creature cannot move left when tile is not passable',
+    false,
+    test.game.pacman.canMove('left')
+  );
+
+  // RIGHT
+
+  test.game.tile(1, 2).wall = false;
+  test.check(
+    'creature can move right when tile is passable',
+    true,
+    test.game.pacman.canMove('right')
+  );
+
+  test.game.tile(1, 2).wall = true;
+  test.check(
+    'creature cannot move right when tile is not passable',
+    false,
+    test.game.pacman.canMove('right')
+  );
+
+  // DOWN
+
+  test.game.tile(2, 1).wall = false;
+  test.check(
+    'creature can move down when tile is passable',
+    true,
+    test.game.pacman.canMove('down')
+  );
+
+  test.game.tile(2, 1).wall = true;
+  test.check(
+    'creature cannot move down when tile is not passable',
+    false,
+    test.game.pacman.canMove('down')
+  );
+});
