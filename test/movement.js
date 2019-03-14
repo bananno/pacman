98c17851b2e21be7773b0dd51cbcc9c7545ddc9d
Anna Peterson <peterson.anna.c@gmail.com>
2019-03-13 20:08:12 -0700

    Add tests for creature movement

--- test/movement.js
+++ test/movement.js
@@ -38,11 +38,31 @@ addTest(test => {
   );
 
   test.game.pacman.position = [1, 1];
+  test.game.pacman.direction = 'up';
+  test.game.tile(0, 1).wall = true;
+  test.game.pacman.move();
+  test.check(
+    'pacman does not move up when up tile is not passable',
+    [1, 1],
+    test.game.pacman.position
+  );
+
+  test.game.pacman.position = [1, 1];
   test.game.pacman.direction = 'left';
   test.game.pacman.move();
   test.check(
     'pacman moves left when direction is left and tile is passable',
     [1, 0],
+    test.game.pacman.position
+  );
+
+  test.game.pacman.position = [1, 1];
+  test.game.pacman.direction = 'left';
+  test.game.tile(1, 0).wall = true;
+  test.game.pacman.move();
+  test.check(
+    'pacman does not move left when left tile is not passable',
+    [1, 1],
     test.game.pacman.position
   );
 
@@ -56,11 +76,31 @@ addTest(test => {
   );
 
   test.game.pacman.position = [1, 1];
+  test.game.pacman.direction = 'right';
+  test.game.tile(1, 2).wall = true;
+  test.game.pacman.move();
+  test.check(
+    'pacman does not move right when right tile is not passable',
+    [1, 1],
+    test.game.pacman.position
+  );
+
+  test.game.pacman.position = [1, 1];
   test.game.pacman.direction = 'down';
   test.game.pacman.move();
   test.check(
     'pacman moves down when direction is down and tile is passable',
     [2, 1],
+    test.game.pacman.position
+  );
+
+  test.game.pacman.position = [1, 1];
+  test.game.pacman.direction = 'down';
+  test.game.tile(2, 1).wall = true;
+  test.game.pacman.move();
+  test.check(
+    'pacman does not move down when down tile is not passable',
+    [1, 1],
     test.game.pacman.position
   );
 });
