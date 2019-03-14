f8e9f22787bb2410e3850c1082388f842600548d
Anna Peterson <peterson.anna.c@gmail.com>
2019-03-14 16:07:37 -0700

    Begin creating separate classes for each ghost

---
+++ scripts/creature/personalities.js
@@ -0,0 +1,28 @@
+
+class Blinky extends Ghost {
+  constructor(game) {
+    super(game);
+    this.color = 'red';
+  }
+}
+
+class Pinky extends Ghost {
+  constructor(game) {
+    super(game);
+    this.color = 'pink';
+  }
+}
+
+class Inky extends Ghost {
+  constructor(game) {
+    super(game);
+    this.color = 'blue';
+  }
+}
+
+class Clyde extends Ghost {
+  constructor(game) {
+    super(game);
+    this.color = 'orange';
+  }
+}
