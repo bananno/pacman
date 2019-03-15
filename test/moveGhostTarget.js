d16e3564e869441909bbafd86ac8bd143b33f165
Anna Peterson <peterson.anna.c@gmail.com>
2019-03-15 12:42:54 -0700

    Add tests for ghosts moving in and out of ghost house

--- test/moveGhostTarget.js
+++ test/moveGhostTarget.js
@@ -1,5 +1,5 @@
 
-// TARGET TILE SELECTION
+// BASIC TARGET TILE SELECTION
 
 addTest(test => {
   test.map = [
@@ -42,6 +42,48 @@ addTest(test => {
   test.check(
     'target tile is the ghost\'s origin when blue ghost is caught',
     [2, 8],
+    test.game.ghosts[0].target
+  );
+});
+
+// TEMPORARY TARGET TILE SELECTION: GHOST HOUSE DOOR
+
+addTest(test => {
+  test.map = [
+    '|||||||||||',
+    '|Ggd      |',
+    '|ggd     t|',
+    '|||||||||||',
+  ];
+
+  test.game.ghosts[0].mode = 'scatter';
+  test.check(
+    'when ghost must exit the ghost house, the temporary target is the first door tile',
+    [1, 3],
+    test.game.ghosts[0].target
+  );
+
+  test.game.ghosts[0].position = [2, 9];
+  test.game.ghosts[0].mode = 'eyes';
+  test.check(
+    'when ghost must enter the ghost house, the temporary target is the first door tile',
+    [1, 3],
+    test.game.ghosts[0].target
+  );
+});
+
+addTest(test => {
+  test.map = [
+    '|||||||||||',
+    '|Ggg      |',
+    '|ggg     t|',
+    '|||||||||||',
+  ];
+
+  test.game.ghosts[0].mode = 'scatter';
+  test.check(
+    'temp temporary target step is skipped if the map has no ghost door tiles',
+    [2, 9],
     test.game.ghosts[0].target
   );
 });
